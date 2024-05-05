d79f66326e0d52ef948c244282857ccd25b773a2
Guilherme-Gomes-Luccas <guilhermegomes.luccas@gmail.com>
2024-05-05 13:57:56 -0300

    adição das aulas de fundamentos do python

---
+++ Conhecendo_linguagem/print_input.py
@@ -0,0 +1,6 @@
+nome = input("Informe o seu nome: ")
+idade = input("Infome sua idade: ")
+
+print(nome, idade)
+print(nome, idade, end="...\n")
+print(nome, idade, sep="#")
---
+++ Conhecendo_linguagem/variaveis_constantes.py
@@ -0,0 +1,12 @@
+nome = "Guilherme"
+idade = 25
+
+nome = "Giovanna"
+
+print(nome, idade)
+
+limite_saque_diario = 1000
+
+STATES = ["SP", "RJ", "SC", "RS"]
+
+print(STATES)
