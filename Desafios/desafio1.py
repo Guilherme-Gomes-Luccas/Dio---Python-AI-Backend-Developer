0a4da12d6188e9971b7353b259518cc5a17fa5c7
Guilherme-Gomes-Luccas <guilhermegomes.luccas@gmail.com>
2024-05-06 17:42:07 -0300

    Fazendo o desafio 1

--- Desafios/desafio1.py
+++ Desafios/desafio1.py
@@ -6,3 +6,37 @@
 # Depósito - Apenas um úsuario
 
 # Extrato - Utilizar append para fazer uma lista que recebe tanto os depósitos quanto os saques
+
+saques = ""
+depositos = ""
+extrato = ""
+
+conta = 0
+deposito = 0
+
+options = "1"
+
+#for number in saque:
+#    if "," in number:
+#        print(f"extrato: {float(extrato):.2f}\n")
+#        extrato = ""
+#    else:
+#        extrato += number
+
+while options != "0":
+    options = input("""
+    ########## BANCO SNAKE ##########
+    #                               #
+    #    O que deseja fazer?        #
+    #                               #
+    #    [1] - Depositar            #
+    #    [2] - Sacar                #
+    #    [3] - Extrato              #
+    #    [0] - Sair                 #
+    #################################
+    O que deseja fazer: """)
+
+    if options == "1":
+        deposito = int(input("Digite a quantidade do deposito: "))
+        depositos += deposito + ","
+    
